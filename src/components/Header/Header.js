e0d88f4242683bbe4a6b709722ab88371ae4fb5e
dytra <vancielnugraha@gmail.com>
2020-03-29 17:44:50 +0700

    add responsive menubar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,32 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import { FaBars,FaTimes } from 'react-icons/fa';
+import React,{useState} from "react"
 import "./Header.scss";
+
 const ListLink = props => (
   <li>
     <Link to={props.to} activeClassName="menu-active">{props.children}</Link>
   </li>
 )
 const Header = ({ siteTitle }) => {
+  const [showMenu,setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  }
   return (<header>
-    <div id="header-container" style={{paddingLeft:'2em'}} >
+    <div id="header-container">
       <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
         <h3 style={{ display: `inline` }}>dytra's note</h3>
       </Link>
-      <ul>
+      <ul className={showMenu ? "force-show" : ""}>
         <ListLink to="/">Home</ListLink>
         <ListLink to="/about/">About</ListLink>
         <ListLink to="/contact/">Contact</ListLink>
         <ListLink to="/portofolio">Portofolio</ListLink>
       </ul>
+  <a id="burger-button" onClick={toggleMenu}>{showMenu ? <FaTimes/> : <FaBars />}</a>
 
     </div>
   </header>)
