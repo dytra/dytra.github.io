66b01c845740622488dcb9e00340eef7996001b0
dytra <vancielnugraha@gmail.com>
2020-03-30 08:12:58 +0700

    add linkedin profile link

--- src/pages/portofolio.js
+++ src/pages/portofolio.js
@@ -7,11 +7,11 @@ export default () => (
   <Layout>
     <SEO title="Portofolio" />
     <h1>Portofolio</h1>
-    <center><img src={constructionImage} alt="In Construction" style={{ maxWidth: '500px',marginBottom:'1em',width:'100%' }} /></center>
+    <center><img src={constructionImage} alt="In Construction" style={{ maxWidth: '500px', marginBottom: '1em', width: '100%' }} /></center>
     <div style={{ textAlign: 'center' }}>
       <h3>Oops.. Sorry</h3>
       <p>This page is under painting...</p>
-      <p>Please check again later</p>
+      <p>Please refer to my <a href="https://www.linkedin.com/in/dytra/">LinkedIn Profile</a> instead.</p>
 
     </div>
   </Layout>
