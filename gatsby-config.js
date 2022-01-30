f099dbfdeab0ff21cd0a3e93a7601bbbe007c601
dytra <vancielnugraha@gmail.com>
2022-01-31 01:00:38 +0700

    add gatsby-plugin-google-fonts-v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: `gatsby-plugin-google-fonts-v2`,
       options: {
         fonts: [
           {
