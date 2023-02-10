27735b974a266f0a11966ab99d0d549f4894934a
dytra <vancielnugraha@gmail.com>
2023-02-10 22:29:28 +0700

    update next config and github workflow

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
 const nextConfig = {
   reactStrictMode: true,
 }
-const repo = 'dytra.github.iov3'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}`
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
 
 module.exports = {
   nextConfig,
