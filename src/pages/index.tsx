2a0dccb6d87ca751d28a5c5cf4835116cc03f280
dytra <vancielnugraha@gmail.com>
2023-02-26 17:41:19 +0700

    add rel="noreferrer" to footer social icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -96,9 +96,9 @@ export default function Home() {
         <div className="spacer h-10 md:h-20"></div>
         <footer className=''>
           <div className='text-gray-500 flex gap-5 justify-center'>
-            <a href="https://twitter.com/dytra_io" target="_blank"><i className="fa-brands fa-twitter text-3xl"></i></a>
-            <a href="https://github.com/dytra" target="_blank"><i className="fa-brands fa-github text-3xl"></i></a>
-            <a href="https://instagram.com/dytra.io" target="_blank"><i className="fa-brands fa-instagram text-3xl"></i></a>
+            <a href="https://twitter.com/dytra_io" target="_blank" rel="noreferrer"><i className="fa-brands fa-twitter text-3xl"></i></a>
+            <a href="https://github.com/dytra" target="_blank" rel="noreferrer"><i className="fa-brands fa-github text-3xl"></i></a>
+            <a href="https://instagram.com/dytra.io" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram text-3xl"></i></a>
           </div>
           <div className='mt-5 text-sm text-gray-500 cursor-default'>
             <p className='text-center'>Copyright © 2022 dytra. All rights reserved.</p>
