1e0941b0498b145903bbb11ea96b36f44143d995
dytra <vancielnugraha@gmail.com>
2024-02-03 07:11:03 +0700

    comment Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,21 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import Hero from "../components/Hero/Hero";
-import MyWorks from "../components/MyWorks";
+import Hero from "../components/Hero/Hero"
+import MyWorks from "../components/MyWorks"
 
 const IndexPage = () => {
   useLayoutEffect(() => {
-    document.location.href="https://dytra.github.io/v3"
-  },[]);
+    document.location.href = "https://dytra.github.io/v3"
+  }, [])
   return (
-    <Layout>
-      {/* <SEO title="Home" />
-      <Hero />
-      <MyWorks /> */}
-
-    </Layout>
+    <>
+      {/* <Layout>
+        <SEO title="Home" />
+        <Hero />
+        <MyWorks />
+      </Layout> */}
+    </>
   )
 }
 
