a35ff62eced6119d54a014eb56dd4a0431570767
dytra <vancielnugraha@gmail.com>
2024-03-10 23:16:09 +0700

    add head on DarkTemplate

--- src/templates/DarkTemplate.tsx
+++ src/templates/DarkTemplate.tsx
@@ -1,4 +1,5 @@
 
+import Head from "next/head";
 import Script from "next/script";
 import React, { ReactNode } from "react";
 
@@ -9,12 +10,17 @@ const DarkTemplate:React.FC<DarkTemplateProps> = ({
   children
 }) => {
   return(
+    <>
+    <Head>
+      <title>dytra.io</title>
+    </Head>
     <div className="bg-black h-screen text-white flex justify-center px-3 md:px-0">
       <div className="container md:w-2/5 space-y-5 pt-5">
       {children}
       </div>
             <Script src="https://kit.fontawesome.com/3d0d822717.js" crossOrigin="anonymous" />
     </div>
+    </>
   )
 }
 
