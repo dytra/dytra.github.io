325174efe7d06fc45d7914af645a5bb75a49d677
dytra <vancielnugraha@gmail.com>
2024-04-12 15:37:34 +0700

    add DotBackground

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Home:React.FC = () => {
   return(
     <DarkTemplate>
-      <h1 className="text-3xl font-bold text-center">Hi there! 👋</h1>
+      <h1 className="text-3xl font-bold   text-center "><span className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">Hi there!</span> <span>👋</span></h1>
       <p className="text-center">I am a Frontend Web Developer with a passion for creating visually stunning, responsive websites. I make user-friendly web-apps that makes our clients happy.</p>
       <div className="container flex justify-center gap-3">
         <Button variant="ghost"><a href="mailto:dytra.io@icloud.com">Connect</a></Button>
@@ -18,5 +18,6 @@ const Home:React.FC = () => {
     </DarkTemplate>
   )
 }
+
 
 export default Home;
--- src/templates/DarkTemplate.tsx
+++ src/templates/DarkTemplate.tsx
@@ -15,13 +15,27 @@ const DarkTemplate:React.FC<DarkTemplateProps> = ({
       <title>dytra.io</title>
     </Head>
     <div className="bg-black h-screen text-white flex justify-center px-3 md:px-0">
+      <DotBackgroundDemo>
       <div className="container md:w-2/5 space-y-5 pt-5">
       {children}
       </div>
             <Script src="https://kit.fontawesome.com/3d0d822717.js" crossOrigin="anonymous" />
+      </DotBackgroundDemo>
     </div>
     </>
   )
 }
-
+const  DotBackgroundDemo = ({children}:{children:ReactNode}) => {
+  return (
+    <div className="h-[50rem] w-full bg-black  bg-dot-white/[0.2] relative flex items-center justify-center">
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center  bg-slate-900 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]">
+      </div>
+     {children} 
+      {/* <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+        Backgrounds
+      </p> */}
+    </div>
+  );
+  }
 export default DarkTemplate;
